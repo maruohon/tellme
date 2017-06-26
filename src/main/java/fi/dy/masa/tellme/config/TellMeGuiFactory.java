0ad98d12d1f28e24cefb381ef73fc53bd9a17832
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2017-06-26 17:10:41 +0300

    Clean up the config GUI stuff

--- src/main/java/fi/dy/masa/tellme/config/TellMeGuiFactory.java
+++ src/main/java/fi/dy/masa/tellme/config/TellMeGuiFactory.java
@@ -1,32 +1,38 @@
 package fi.dy.masa.tellme.config;
 
-import java.util.Set;
-import net.minecraft.client.Minecraft;
+import java.util.ArrayList;
+import java.util.List;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraftforge.fml.client.IModGuiFactory;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.fml.client.DefaultGuiFactory;
+import net.minecraftforge.fml.client.config.GuiConfig;
+import net.minecraftforge.fml.client.config.IConfigElement;
+import fi.dy.masa.tellme.reference.Reference;
 
-public class TellMeGuiFactory implements IModGuiFactory
+public class TellMeGuiFactory extends DefaultGuiFactory
 {
-    @Override
-    public void initialize(Minecraft minecraftInstance)
+    public TellMeGuiFactory()
     {
+        super(Reference.MOD_ID, getTitle());
     }
 
     @Override
-    public boolean hasConfigGui()
+    public GuiScreen createConfigGui(GuiScreen parent)
     {
-        return true;
+        return new GuiConfig(parent, getConfigElements(), Reference.MOD_ID, false, false, getTitle());
     }
 
-    @Override
-    public GuiScreen createConfigGui(GuiScreen parentScreen)
+    private static List<IConfigElement> getConfigElements()
     {
-        return new TellMeConfigGui(parentScreen);
+        List<IConfigElement> configElements = new ArrayList<IConfigElement>();
+
+        configElements.addAll(new ConfigElement(Configs.config.getCategory(Configs.CATEGORY_GENERIC)).getChildElements());
+
+        return configElements;
     }
 
-    @Override
-    public Set<RuntimeOptionCategoryElement> runtimeGuiCategories()
+    private static String getTitle()
     {
-        return null;
+        return GuiConfig.getAbridgedConfigPath(Configs.configurationFile.toString());
     }
 }
