eae09e1c5a96f18adc06ac972ef58bf04c06a499
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2017-09-15 00:17:58 +0300

    Bump version to 1.12.1-0.5.4. Sign the JAR.

--- src/main/java/fi/dy/masa/tellme/TellMe.java
+++ src/main/java/fi/dy/masa/tellme/TellMe.java
@@ -1,10 +1,12 @@
 package fi.dy.masa.tellme;
 
 import java.io.File;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import fi.dy.masa.tellme.command.CommandTellme;
@@ -13,11 +15,11 @@ import fi.dy.masa.tellme.event.InteractEventHandler;
 import fi.dy.masa.tellme.proxy.CommonProxy;
 import fi.dy.masa.tellme.reference.Reference;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION,
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, certificateFingerprint = Reference.FINGERPRINT,
     guiFactory = "fi.dy.masa.tellme.config.TellMeGuiFactory",
     acceptableRemoteVersions = "*",
-    acceptedMinecraftVersions = "1.12",
-    dependencies = "required-after:forge@[14.21.0.2363,);")
+    dependencies = "required-after:forge@[14.21.0.2363,);",
+    acceptedMinecraftVersions = "[1.12,1.12.2]")
 public class TellMe
 {
     @Mod.Instance(Reference.MOD_ID)
@@ -26,13 +28,12 @@ public class TellMe
     @SidedProxy(clientSide = Reference.PROXY_CLASS_CLIENT, serverSide = Reference.PROXY_CLASS_SERVER)
     public static CommonProxy proxy;
 
-    public static Logger logger;
+    public static final Logger logger = LogManager.getLogger(Reference.MOD_ID);
     public static String configDirPath;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-        logger = event.getModLog();
         configDirPath = new File(event.getModConfigurationDirectory(), Reference.MOD_ID).getAbsolutePath();
         Configs.loadConfigsFromFile(event.getSuggestedConfigurationFile());
 
@@ -45,5 +46,24 @@ public class TellMe
     public void serverStarting(FMLServerStartingEvent event)
     {
         event.registerServerCommand(new CommandTellme());
+    }
+
+    @Mod.EventHandler
+    public void onFingerPrintViolation(FMLFingerprintViolationEvent event)
+    {
+        // Not running in a dev environment
+        if (event.isDirectory() == false)
+        {
+            logger.warn("*********************************************************************************************");
+            logger.warn("*****                                    WARNING                                        *****");
+            logger.warn("*****                                                                                   *****");
+            logger.warn("*****   The signature of the mod file '{}' does not match the expected fingerprint!     *****", event.getSource().getName());
+            logger.warn("*****   This might mean that the mod file has been tampered with!                       *****");
+            logger.warn("*****   If you did not download the mod {} directly from Curse/CurseForge,       *****", Reference.MOD_NAME);
+            logger.warn("*****   or using one of the well known launchers, and you did not                       *****");
+            logger.warn("*****   modify the mod file at all yourself, then it's possible,                        *****");
+            logger.warn("*****   that it may contain malware or other unwanted things!                           *****");
+            logger.warn("*********************************************************************************************");
+        }
     }
 }
--- src/main/java/fi/dy/masa/tellme/reference/Reference.java
+++ src/main/java/fi/dy/masa/tellme/reference/Reference.java
@@ -5,6 +5,7 @@ public class Reference
     public static final String MOD_ID = "tellme";
     public static final String MOD_NAME = "Tell Me";
     public static final String MOD_VERSION = "@MOD_VERSION@";
+    public static final String FINGERPRINT = "@FINGERPRINT@";
     public static final String PROXY_CLASS_CLIENT = "fi.dy.masa.tellme.proxy.ClientProxy";
     public static final String PROXY_CLASS_SERVER = "fi.dy.masa.tellme.proxy.CommonProxy";
 }
