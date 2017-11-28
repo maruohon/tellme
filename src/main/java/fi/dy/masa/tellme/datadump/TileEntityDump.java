0e8b0e6f859d7c6b8555e6c33df4a455a9003459
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2017-11-28 05:19:35 +0200

    Add a "Ticking?" column to the TE dump

--- src/main/java/fi/dy/masa/tellme/datadump/TileEntityDump.java
+++ src/main/java/fi/dy/masa/tellme/datadump/TileEntityDump.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Set;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ITickable;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.registry.RegistryNamespaced;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
@@ -15,7 +16,7 @@ public class TileEntityDump extends DataDump
 
     private TileEntityDump(Format format)
     {
-        super(2, format);
+        super(3, format);
     }
 
     public static List<String> getFormattedTileEntityDump(Format format)
@@ -30,10 +31,11 @@ public class TileEntityDump extends DataDump
             for (ResourceLocation key : keys)
             {
                 Class <? extends TileEntity> clazz = registry.getObject(key);
-                tileEntityDump.addData(clazz.getName(), key.toString());
+                tileEntityDump.addData(clazz.getName(), key.toString(), ITickable.class.isAssignableFrom(clazz) ? "yes" : "-");
             }
 
-            tileEntityDump.addTitle("Class", "Registry name");
+            tileEntityDump.addTitle("Class", "Registry name", "Ticking?");
+            tileEntityDump.setColumnAlignment(2, Alignment.RIGHT);
             tileEntityDump.setUseColumnSeparator(true);
         }
         catch (Exception e)
