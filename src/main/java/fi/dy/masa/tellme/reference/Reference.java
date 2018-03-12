a41bda0a94149c8c897cdb304f1662d2483e3b95
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2018-03-13 00:37:01 +0200

    Remove unused Reference strings

--- src/main/java/fi/dy/masa/tellme/reference/Reference.java
+++ src/main/java/fi/dy/masa/tellme/reference/Reference.java
@@ -5,7 +5,4 @@ public class Reference
     public static final String MOD_ID = "tellme";
     public static final String MOD_NAME = "TellMe";
     public static final String MOD_VERSION = "@MOD_VERSION@";
-    public static final String FINGERPRINT = "@FINGERPRINT@";
-    public static final String PROXY_CLASS_CLIENT = "fi.dy.masa.tellme.proxy.ClientProxy";
-    public static final String PROXY_CLASS_SERVER = "fi.dy.masa.tellme.proxy.CommonProxy";
 }
