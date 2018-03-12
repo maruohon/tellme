bcbb31a42e25f165f402936b8984746a881a686d
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2018-03-12 17:29:12 +0200

    Improve/clean up the tab completion related Mixins

--- src/main/java/fi/dy/masa/tellme/mixin/MixinTabCompleter.java
+++ src/main/java/fi/dy/masa/tellme/mixin/MixinTabCompleter.java
@@ -1,31 +1,21 @@
 package fi.dy.masa.tellme.mixin;
 
 import java.util.List;
-import org.spongepowered.asm.lib.Opcodes;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import fi.dy.masa.tellme.command.ClientCommandHandler;
-import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.util.TabCompleter;
 
 @Mixin(TabCompleter.class)
 public class MixinTabCompleter
 {
     @Shadow
-    protected boolean requestedCompletions;
-    @Shadow
-    protected boolean didComplete;
-    @Shadow
     protected List<String> completions;
-    @Shadow
-    @Final
-    protected GuiTextField textField;
-    @Shadow
-    protected int completionIdx;
 
     @Shadow
     public void complete() {}
@@ -41,60 +31,39 @@ public class MixinTabCompleter
         }
     }
 
-    @Inject(method = "setCompletions", at = @At("HEAD"), cancellable = true)
-    private void onSetCompletions(String[] newCompl, CallbackInfo ci)
+    @Redirect(method = "setCompletions", at = @At(
+                value = "INVOKE",
+                target = "Lorg/apache/commons/lang3/StringUtils;getCommonPrefix([Ljava/lang/String;)V"
+        ))
+    public String addCompletionsAndRemoveFormattingCodes(String... newCompl)
     {
-        if (this.requestedCompletions)
+        String[] complete = ClientCommandHandler.INSTANCE.latestAutoComplete;
+
+        // Add our command completions
+        if (complete != null)
         {
-            this.didComplete = false;
-            this.completions.clear();
-
-            String[] complete = ClientCommandHandler.INSTANCE.latestAutoComplete;
-
-            if (complete != null)
+            for (String s : complete)
             {
-                newCompl = com.google.common.collect.ObjectArrays.concat(complete, newCompl, String.class);
-            }
-
-            for (String s : newCompl)
-            {
-                if (!s.isEmpty())
+                if (s.isEmpty() == false)
                 {
                     this.completions.add(s);
                 }
             }
 
-            String s1 = this.textField.getText().substring(this.textField.getNthWordFromPosWS(-1, this.textField.getCursorPosition(), false));
-            String s2 = org.apache.commons.lang3.StringUtils.getCommonPrefix(newCompl);
-            s2 = net.minecraft.util.text.TextFormatting.getTextWithoutFormattingCodes(s2);
-
-            if (!s2.isEmpty() && !s1.equalsIgnoreCase(s2))
-            {
-                this.textField.deleteFromCursor(0);
-                this.textField.deleteFromCursor(this.textField.getNthWordFromPosWS(-1, this.textField.getCursorPosition(), false) - this.textField.getCursorPosition());
-                this.textField.writeText(s2);
-            }
-            else if (!this.completions.isEmpty())
-            {
-                this.didComplete = true;
-                this.complete();
-            }
-
-            ci.cancel();
+            newCompl = com.google.common.collect.ObjectArrays.concat(complete, newCompl, String.class);
         }
+
+        String prefix = org.apache.commons.lang3.StringUtils.getCommonPrefix(newCompl);
+
+        return net.minecraft.util.text.TextFormatting.getTextWithoutFormattingCodes(prefix);
     }
 
-    // This injection point is before the completionIdx gets incremented at the end of the complete() method.
-    @Inject(method = "complete", at = @At(
-                //value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiTextField;writeText(Ljava/lang/String;)V"
-                value = "FIELD", target = "Lnet/minecraft/util/TabCompleter;completionIdx:I", opcode = Opcodes.PUTFIELD, ordinal = 2
-            ),
-            cancellable = true)
-    private void onComplete(CallbackInfo ci)
+    @ModifyArg(method = "complete", at = @At(
+                value = "INVOKE",
+                target = "Lnet/minecraft/client/gui/GuiTextField;writeText(Ljava/lang/String;)V"
+            ))
+    public String removeFormattingCodes2(String text)
     {
-        String cleaned = this.completions.get(this.completionIdx++);
-        cleaned = net.minecraft.util.text.TextFormatting.getTextWithoutFormattingCodes(cleaned);
-        this.textField.writeText(cleaned);
-        ci.cancel();
+        return net.minecraft.util.text.TextFormatting.getTextWithoutFormattingCodes(text);
     }
 }
