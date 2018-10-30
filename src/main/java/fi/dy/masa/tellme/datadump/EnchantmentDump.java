b358449a700ae5020a70d8df2cd364826fcb5f21
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2018-10-30 15:27:50 +0200

    Protect against null values in the Enchantments dump. Fixes #13.

--- src/main/java/fi/dy/masa/tellme/datadump/EnchantmentDump.java
+++ src/main/java/fi/dy/masa/tellme/datadump/EnchantmentDump.java
@@ -1,8 +1,7 @@
 package fi.dy.masa.tellme.datadump;
 
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
@@ -17,19 +16,22 @@ public class EnchantmentDump extends DataDump
     public static List<String> getFormattedEnchantmentDump(Format format)
     {
         EnchantmentDump enchantmentDump = new EnchantmentDump(format);
-        Iterator<Map.Entry<ResourceLocation, Enchantment>> iter = ForgeRegistries.ENCHANTMENTS.getEntries().iterator();
+        Set<ResourceLocation> keys = ForgeRegistries.ENCHANTMENTS.getKeys();
 
-        while (iter.hasNext())
+        for (ResourceLocation key : keys)
         {
-            Map.Entry<ResourceLocation, Enchantment> entry = iter.next();
-            Enchantment ench = entry.getValue();
-            String regName = entry.getKey().toString();
-            String name = ench.getName();
-            String type = ench.type.toString();
-            String rarity = ench.getRarity().toString();
-            int id = Enchantment.getEnchantmentID(ench);
+            Enchantment ench = ForgeRegistries.ENCHANTMENTS.getValue(key);
 
-            enchantmentDump.addData(regName, name, type, rarity, String.valueOf(id));
+            if (ench != null)
+            {
+                String regName = key.toString();
+                String name = ench.getName() != null ? ench.getName() : "<null>";
+                String type = ench.type != null ? ench.type.toString() : "<null>";
+                String rarity = ench.getRarity() != null ? ench.getRarity().toString() : "<null>";
+                int id = Enchantment.getEnchantmentID(ench);
+
+                enchantmentDump.addData(regName, name, type, rarity, String.valueOf(id));
+            }
         }
 
         enchantmentDump.addTitle("Registry name", "Name", "Type", "Rarity", "ID");
