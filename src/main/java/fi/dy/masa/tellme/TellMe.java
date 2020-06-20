98a325973a3cd0a5cdd918532b28f6b0c94b5e49
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2020-06-20 03:05:03 +0300

    Fix the commands not being registered on a dedicated server, plus a crash

--- src/main/java/fi/dy/masa/tellme/TellMe.java
+++ src/main/java/fi/dy/masa/tellme/TellMe.java
@@ -1,16 +1,8 @@
 package fi.dy.masa.tellme;
 
-import java.util.Collections;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import net.minecraft.command.arguments.ArgumentTypes;
-import net.minecraft.command.arguments.serialize.ConstantArgumentSerializer;
-import fi.dy.masa.tellme.command.argument.BiomeArgument;
-import fi.dy.masa.tellme.command.argument.FileArgument;
-import fi.dy.masa.tellme.command.argument.GroupingArgument;
-import fi.dy.masa.tellme.command.argument.OutputFormatArgument;
-import fi.dy.masa.tellme.command.argument.OutputTypeArgument;
-import fi.dy.masa.tellme.command.argument.StringCollectionArgument;
+import fi.dy.masa.tellme.command.CommandTellMe;
 import fi.dy.masa.tellme.datadump.DataProviderBase;
 import fi.dy.masa.tellme.datadump.DataProviderClient;
 import fi.dy.masa.tellme.reference.Reference;
@@ -19,30 +11,19 @@ import net.fabricmc.api.ModInitializer;
 public class TellMe implements ModInitializer
 {
     public static final Logger logger = LogManager.getLogger(Reference.MOD_ID);
-    public static DataProviderBase dataProvider;
+    public static DataProviderBase dataProvider = new DataProviderBase();
     private static boolean isClient;
-
-    public TellMe()
-    {
-        dataProvider = new DataProviderBase();
-
-        ArgumentTypes.register("tellme:biome", BiomeArgument.class, new ConstantArgumentSerializer<>(BiomeArgument::create));
-        ArgumentTypes.register("tellme:file", FileArgument.class, new ConstantArgumentSerializer<>(FileArgument::createEmpty));
-        ArgumentTypes.register("tellme:grouping", GroupingArgument.class, new ConstantArgumentSerializer<>(GroupingArgument::create));
-        ArgumentTypes.register("tellme:output_format", OutputFormatArgument.class, new ConstantArgumentSerializer<>(OutputFormatArgument::create));
-        ArgumentTypes.register("tellme:output_type", OutputTypeArgument.class, new ConstantArgumentSerializer<>(OutputTypeArgument::create));
-        ArgumentTypes.register("tellme:string_collection", StringCollectionArgument.class, new ConstantArgumentSerializer<>(() -> StringCollectionArgument.create(() -> Collections.emptyList(), "")));
-    }
 
     @Override
     public void onInitialize()
     {
+        CommandTellMe.registerArgumentTypes();
     }
 
     public static void setIsClient()
     {
-        isClient = true;
         dataProvider = new DataProviderClient();
+        isClient = true;
     }
 
     public static boolean isClient()
--- src/main/java/fi/dy/masa/tellme/command/CommandTellMe.java
+++ src/main/java/fi/dy/masa/tellme/command/CommandTellMe.java
@@ -1,9 +1,18 @@
 package fi.dy.masa.tellme.command;
 
+import java.util.Collections;
 import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.command.arguments.ArgumentTypes;
+import net.minecraft.command.arguments.serialize.ConstantArgumentSerializer;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.LiteralText;
+import fi.dy.masa.tellme.command.argument.BiomeArgument;
+import fi.dy.masa.tellme.command.argument.FileArgument;
+import fi.dy.masa.tellme.command.argument.GroupingArgument;
+import fi.dy.masa.tellme.command.argument.OutputFormatArgument;
+import fi.dy.masa.tellme.command.argument.OutputTypeArgument;
+import fi.dy.masa.tellme.command.argument.StringCollectionArgument;
 
 public class CommandTellMe
 {
@@ -15,6 +24,16 @@ public class CommandTellMe
     public static void registerClientCommand(CommandDispatcher<ServerCommandSource> dispatcher)
     {
         register(dispatcher, "ctellme", 0);
+    }
+
+    public static void registerArgumentTypes()
+    {
+        ArgumentTypes.register("tellme:biome", BiomeArgument.class, new ConstantArgumentSerializer<>(BiomeArgument::create));
+        ArgumentTypes.register("tellme:file", FileArgument.class, new ConstantArgumentSerializer<>(FileArgument::createEmpty));
+        ArgumentTypes.register("tellme:grouping", GroupingArgument.class, new ConstantArgumentSerializer<>(GroupingArgument::create));
+        ArgumentTypes.register("tellme:output_format", OutputFormatArgument.class, new ConstantArgumentSerializer<>(OutputFormatArgument::create));
+        ArgumentTypes.register("tellme:output_type", OutputTypeArgument.class, new ConstantArgumentSerializer<>(OutputTypeArgument::create));
+        ArgumentTypes.register("tellme:string_collection", StringCollectionArgument.class, new ConstantArgumentSerializer<>(() -> StringCollectionArgument.create(() -> Collections.emptyList(), "")));
     }
 
     protected static void register(CommandDispatcher<ServerCommandSource> dispatcher, String baseCommandName, int permissionLevel)
