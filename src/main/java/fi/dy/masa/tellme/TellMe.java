839fbff8c1fb2c5e743c3b72ee41267d40fba03e
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2023-12-27 00:18:22 +0200

    Port to Ornithe 1.12.2

--- src/main/java/fi/dy/masa/tellme/TellMe.java
+++ src/main/java/fi/dy/masa/tellme/TellMe.java
@@ -1,13 +1,35 @@
 package fi.dy.masa.tellme;
 
+import net.ornithemc.osl.entrypoints.api.ModInitializer;
+import net.ornithemc.osl.lifecycle.api.client.MinecraftClientEvents;
+import net.ornithemc.osl.lifecycle.api.server.MinecraftServerEvents;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.minecraft.command.ServerCommandManager;
+
+import malilib.config.util.ConfigUtils;
+import malilib.registry.Registry;
+import fi.dy.masa.tellme.command.ClientCommandTellme;
+import fi.dy.masa.tellme.command.CommandTellme;
 import fi.dy.masa.tellme.reference.Reference;
 
-public class TellMe
+public class TellMe implements ModInitializer
 {
     public static final Logger LOGGER = LogManager.getLogger(Reference.MOD_ID);
 
     public static String configDirPath;
+
+    @Override
+    public void init()
+    {
+        MinecraftClientEvents.READY.register(mc -> onClientReady());
+        MinecraftServerEvents.READY.register(srv -> ((ServerCommandManager) srv.getCommandManager()).registerCommand(new CommandTellme()));
+        Registry.CLIENT_COMMAND_HANDLER.registerCommand(new ClientCommandTellme());
+    }
+
+    private static void onClientReady()
+    {
+        configDirPath = ConfigUtils.getConfigDirectory().resolve(Reference.MOD_ID).toFile().getAbsolutePath();
+    }
 }
