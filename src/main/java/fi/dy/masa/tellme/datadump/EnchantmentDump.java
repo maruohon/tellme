01c3bb4a31395601cdcc945385665e24df1b7d9e
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2024-07-23 03:22:51 +0300

    Add the max level to the enchantment dump

--- src/main/java/fi/dy/masa/tellme/datadump/EnchantmentDump.java
+++ src/main/java/fi/dy/masa/tellme/datadump/EnchantmentDump.java
@@ -12,7 +12,7 @@ public class EnchantmentDump
 {
     public static List<String> getFormattedEnchantmentDump(DataDump.Format format)
     {
-        DataDump enchantmentDump = new DataDump(5, format);
+        DataDump enchantmentDump = new DataDump(6, format);
 
         for (Identifier key : Registries.ENCHANTMENT.getIds())
         {
@@ -22,17 +22,18 @@ public class EnchantmentDump
             {
                 String regName = key.toString();
                 String name = ench.getTranslationKey() != null ? ench.getTranslationKey() : "<null>";
+                String maxLvl = String.valueOf(ench.getMaxLevel());
                 String target = ench.target != null ? ench.target.toString() : "<null>";
                 Enchantment.Rarity rarity = ench.getRarity();
                 String rarityStr = rarity != null ? String.format("%s (%d)", rarity, rarity.getWeight()) : "<null>";
                 int intId = Registries.ENCHANTMENT.getRawId(ench);
 
-                enchantmentDump.addData(regName, name, target, rarityStr, String.valueOf(intId));
+                enchantmentDump.addData(regName, name, maxLvl, target, rarityStr, String.valueOf(intId));
             }
         }
 
-        enchantmentDump.addTitle("Registry name", "Name", "Target", "Rarity", "ID");
-        enchantmentDump.setColumnProperties(4, DataDump.Alignment.RIGHT, true); // id
+        enchantmentDump.addTitle("Registry name", "Name", "Max lvl", "Target", "Rarity", "ID");
+        enchantmentDump.setColumnProperties(5, DataDump.Alignment.RIGHT, true); // id
 
         return enchantmentDump.getLines();
     }
